d1af9e2de62e929499d90f0bb5a9a60a27b460f2
insan firmansyah <ichan.chanliezta@gmail.com>
2016-09-26 21:56:51 +0700

    initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name webBantuhunaMinifyApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the webBantuhunaMinifyApp
+ */
+angular.module('webBantuhunaMinifyApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name webBantuhunaMinifyApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the webBantuhunaMinifyApp
+ */
+angular.module('webBantuhunaMinifyApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
